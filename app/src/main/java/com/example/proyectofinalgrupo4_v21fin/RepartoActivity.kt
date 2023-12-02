21b9ba5a64af24ee472170b6981196b2fae44fca
OSalazar <18200010@ue.edu.pe>
2023-12-02 08:15:26 -0500

    btnback

--- app/src/main/java/com/example/proyectofinalgrupo4_v21fin/RepartoActivity.kt
+++ app/src/main/java/com/example/proyectofinalgrupo4_v21fin/RepartoActivity.kt
@@ -2,10 +2,17 @@ package com.example.proyectofinalgrupo4_v21fin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RepartoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reparto)
+
+        val btnBack:Button = findViewById(R.id.btnBackMap)
+
+        btnBack.setOnClickListener {
+
+        }
     }
 }
