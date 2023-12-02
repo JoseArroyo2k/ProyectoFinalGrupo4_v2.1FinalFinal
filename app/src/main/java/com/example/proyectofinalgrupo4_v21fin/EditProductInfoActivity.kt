949016eca3de8f3aabc458332fb8c6496b756701
OSalazar <18200010@ue.edu.pe>
2023-12-02 07:35:43 -0500

    Update EditProductInfoActivity.kt

--- app/src/main/java/com/example/proyectofinalgrupo4_v21fin/EditProductInfoActivity.kt
+++ app/src/main/java/com/example/proyectofinalgrupo4_v21fin/EditProductInfoActivity.kt
@@ -2,10 +2,57 @@ package com.example.proyectofinalgrupo4_v21fin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
 
 class EditProductInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_product_info)
+
+        val btnBack: Button = findViewById(R.id.btnBackCPN)
+        val txtNameProd: EditText = findViewById(R.id.ProdName)
+        val txtDescProd: EditText = findViewById(R.id.editTextTextMultiLine)
+        val txtPrice: EditText = findViewById(R.id.editTextTextPrice)
+
+
+        val btnSaveEdit: Button = findViewById(R.id.btnSaveEditProd)
+
+        val auth = FirebaseAuth.getInstance()
+        val db = FirebaseFirestore.getInstance()
+
+        val collectionRef = db.collection("Productos")
+
+        btnSaveEdit.setOnClickListener {
+            val nombre = txtNameProd.text.toString()
+            val desc = txtDescProd.text.toString()
+            val precio = txtPrice.text.toString()
+
+            db.collection("Productos").addSnapshotListener{snapshots,e->
+                if (e!=null){
+                    Log.w("Firebase","listen:error",e)
+                    return@addSnapshotListener
+                }
+                for (dc in snapshots!!.documentChanges){
+                    when (dc.type){
+                        DocumentChange.Type.ADDED ->{
+                            Log.d("Firebase","Data: "+dc.document.data)
+
+                        }
+                        DocumentChange.Type.MODIFIED -> {
+
+                        }
+                        DocumentChange.Type.REMOVED -> {
+
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
